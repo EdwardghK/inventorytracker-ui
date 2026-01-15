873d352bf73bf95abae7cb3af914da38c12b867d
Edward Kwong <kwon0032@algonquinlive.com>
2026-01-15 10:20:29 -0500

    worker file update

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,11 @@
 export default {
   async fetch(request, env, ctx) {
-    return env.ASSETS.fetch(request);
+    const url = new URL(request.url);
+    const isAsset = url.pathname.includes('.') || url.pathname.startsWith('/assets/');
+    if (isAsset) {
+      return env.ASSETS.fetch(request);
+    }
+    const indexRequest = new Request(new URL('/index.html', url), request);
+    return env.ASSETS.fetch(indexRequest);
   },
 };
