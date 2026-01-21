258e6a6b462fde8e4fc944f5de5a9124b2203cbd
Edward Kwong <kwon0032@algonquinlive.com>
2026-01-21 01:58:52 -0500

    simplify the UI for main page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { InventoryManageComponent } from './inventory/manage.component';
 import { DrinkSpecComponent } from './drinks/drink-spec.component';
 
 export const routes: Routes = [
   { path: '', component: DrinkSpecComponent },
-  { path: 'dashboard', component: DashboardComponent },
   { path: 'inventory', component: InventoryManageComponent },
   { path: 'drink-spec', component: DrinkSpecComponent },
   { path: '**', redirectTo: '' },
